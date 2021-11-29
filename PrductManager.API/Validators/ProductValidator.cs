6339da24cbd7c2174da799d3b233a5313b64c653
Homa Pourmohammadi <homa.pourmohammadi@gmail.com>
2021-11-29 04:15:53 +0100

    Product validator

---
+++ PrductManager.API/Validators/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using ProductManager.Domain.Models;
+
+namespace ProductManager.API.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            // Double validations through DataAnnotations and FluentValidation is not a good practice
+            // and only for demonstration purposes have been put here.
+            RuleFor(m => m.Name)
+                .NotEmpty()
+                .WithMessage("Name should not be empty.")
+                .Length(3, 50);
+            RuleFor(x => x.Price).Must(x => x > 0)
+                .WithMessage("Price should be bigger than 0");
+            RuleFor(x => x.Type).Must(x => x > 0)
+                .WithMessage("Product Type should be selected.");
+            RuleFor(x => x.Category).Must(x => x != null)
+                .WithMessage("Product Category should be selected.");
+        }
+    }
+}
