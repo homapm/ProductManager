5691ea30dcd2661bbf9d41f321278777a5c5ec90
Homa Pourmohammadi <homa.pourmohammadi@gmail.com>
2021-11-29 03:30:11 +0100

    Add repositories

---
+++ ProductManager.Domain/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Domain.Contracts
+{
+    public interface IRepository<T>
+    {
+        public IEnumerable<T> GetAll();
+
+        public T GetById(int id);
+        public Task<T> Create(T @object);
+
+        public void Update(T @object);
+
+        public void Delete(T @object);
+    }
+}
---
+++ ProductManager.Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductManager.Domain.Contracts;
+using ProductManager.Domain.Data;
+using ProductManager.Domain.Models;
+
+namespace ProductManager.Domain.Repositories
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        readonly ProductManagerDbContext _dbContext;
+        public ProductRepository(ProductManagerDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _dbContext.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public async Task<Product> Create(Product @object)
+        {
+            var obj = await _dbContext.Products.AddAsync(@object);
+            await _dbContext.SaveChangesAsync();
+            return obj.Entity;
+        }
+
+        public void Update(Product @object)
+        {
+            _dbContext.Products.Update(@object);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(Product @object)
+        {
+            _dbContext.Remove(@object);
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
